10117bf77930251434d571edb6bba4aad9f021bf
shantanu-bit <81363096+shantanu-bit@users.noreply.github.com>
2024-09-18 12:03:57 +0530

    Update student.py

--- student.py
+++ student.py
@@ -109,5 +109,5 @@ def ask():
 
 # Change in app.run() for Render deployment
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))  # Get port from environment or default to 5000
+    port = int(os.environ.get('PORT', 5000))  # Get port from environment or default to 5000
     application.run(host='0.0.0.0', port=port, debug=False)
